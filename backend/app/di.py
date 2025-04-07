fbc731acfc61a7bd9b088bb27c0cd77357bf9eaa
mrKazzila <mrkazzila@gmail.com>
2025-04-08 02:13:03 +0300

    fix: remove unused provider and update scope

--- backend/app/di.py
+++ backend/app/di.py
@@ -1,7 +1,6 @@
 from dishka import Provider, Scope, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.adapters.domain.urls_repository import UrlsRepository
 from app.service_layer.services.urls import UrlsServices
 from app.service_layer.unit_of_work import UnitOfWork
 from app.settings.database import async_session_maker
@@ -14,16 +13,10 @@ class ServiceProvider(Provider):
     async def provide_async_session(self) -> AsyncSession:
         return async_session_maker()
 
-    @provide(scope=Scope.REQUEST)
-    def provide_urls_repo(self, session: AsyncSession) -> UrlsRepository:
-        return UrlsRepository(session=session)
+    @provide(scope=Scope.APP)
+    def provide_uow(self) -> UnitOfWork:
+        return UnitOfWork(session_factory=async_session_maker)
 
-    @provide(scope=Scope.REQUEST)
-    def provide_uow(self, session: AsyncSession) -> UnitOfWork:
-        uow = UnitOfWork()
-        uow._session = session
-        return uow
-
-    @provide(scope=Scope.REQUEST)
+    @provide(scope=Scope.APP)
     def provide_url_service(self, uow: UnitOfWork) -> UrlsServices:
         return UrlsServices(uow=uow)
