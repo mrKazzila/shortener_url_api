802e565115d292e2aa4e8a7412859d9330cadd52
mrKazzila <mrkazzila@gmail.com>
2025-12-01 14:59:07 +0300

    refactor: add prefix and new endpoints

--- backend/src/presentation/api/rest/routers/users/routers.py
+++ backend/src/presentation/api/rest/routers/users/routers.py
@@ -1,9 +1,10 @@
 __all__ = ("router",)
 
+from typing import Annotated
 from uuid import UUID
 
 from dishka.integrations.fastapi import DishkaRoute, FromDishka
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 
 from src.application.dtos.users import (
     # noqa
@@ -11,10 +12,15 @@ from src.application.dtos.users import (
     PaginationDTO,
 )
 from src.application.use_cases import GetUserUrlsUseCase
+from src.presentation.api.schemas.pagination import (
+    PaginationParams,
+    pagination_params,
+)
 from src.presentation.api.schemas.users import SUserUrls
 from src.presentation.mappers.user_mapper import UserPresentationMapper
 
 router: APIRouter = APIRouter(
+    prefix="/users",
     tags=["users"],
     route_class=DishkaRoute,
 )
@@ -29,6 +35,7 @@ async def get_user_urls(
     # header: FromDishka[XUserHeaderDTO],
     use_case: FromDishka[GetUserUrlsUseCase],
     mapper: FromDishka[UserPresentationMapper],
+    pagination: Annotated[PaginationParams, Depends(pagination_params)],
 ) -> SUserUrls:
     """Get all user urls."""
     user_urls = await use_case.execute(
@@ -36,9 +43,8 @@ async def get_user_urls(
             # user_id=UUID(header.x_user_id),
             user_id=UUID("3b0e3fe7-e753-4e14-9ff4-0a200c2cbdcf"),
             pagination_data=PaginationDTO(
-                limit=10,
-                offset=0,
-                skip=0,
+                limit=pagination.limit,
+                last_id=pagination.last_id,
             ),
         ),
     )
