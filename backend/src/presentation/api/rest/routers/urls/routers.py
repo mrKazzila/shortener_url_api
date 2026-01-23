d2b68c084c1c8b7cafc018fc804bee1eff368545
mrKazzila <mrkazzila@gmail.com>
2026-01-23 13:07:10 +0300

    feat: add new methods for urls

--- backend/src/presentation/api/rest/routers/urls/routers.py
+++ backend/src/presentation/api/rest/routers/urls/routers.py
@@ -6,15 +6,20 @@ from dishka.integrations.fastapi import DishkaRoute, FromDishka
 from fastapi import APIRouter, status
 from fastapi.responses import RedirectResponse
 
-from src.application.dtos.urls import CreateUrlDTO
+from src.application.dtos.urls import CreateUrlDTO, DeleteUrlDTO, UpdateUrlDTO
 from src.application.dtos.users import XUserHeaderDTO  # noqa
 from src.application.use_cases import (
     CreateUrlUseCase,
     RedirectToOriginalUrlUseCase,
+    DeleteUrlUseCase,
+    UpdateUrlUseCase,
+
 )
 from src.presentation.api.rest.routers.urls._types import (
     PathUrlKey,
     QueryLongUrl,
+    QueryUrlIsActive,
+    QueryUrlName,
 )
 from src.presentation.api.schemas.urls import SUrlResponse
 from src.presentation.exceptions.urls import UrlNotFoundException
@@ -75,27 +80,40 @@ async def redirect_to_target_url(
     status_code=status.HTTP_201_CREATED,
 )
 async def update_url(
-    url: QueryLongUrl,
+    url_key: PathUrlKey,
+    is_active: QueryUrlIsActive,
+    url_name: QueryUrlName,
     # header: FromDishka[XUserHeaderDTO],
-    # use_case: FromDishka[CreateUrlUseCase],
+    use_case: FromDishka[UpdateUrlUseCase],
     # mapper: FromDishka[UrlPresentationMapper],
-) -> SUrlResponse:
+):
     """Creates a shortened URL."""
-    # Fixme: implement update logic
-    raise NotImplementedError("TODO: implement update logic")
+    if await use_case.execute(
+        dto=UpdateUrlDTO(
+            key=url_key,
+            is_active=is_active,
+            name=url_name,
+        ),
+    ):
+        return {"updated": True}
+
+    raise UrlNotFoundException(url_key=url_key)
 
 
 @router.delete(
     path="/{url_key}",
     name="Remove url",
-    status_code=status.HTTP_201_CREATED,
+    status_code=status.HTTP_204_NO_CONTENT,
 )
 async def delete_url(
-    url: QueryLongUrl,
+    url_key: PathUrlKey,
     # header: FromDishka[XUserHeaderDTO],
-    # use_case: FromDishka[CreateUrlUseCase],
-    # mapper: FromDishka[UrlPresentationMapper],
-) -> SUrlResponse:
-    """Creates a shortened URL."""
-    # Fixme: implement delete logic
-    raise NotImplementedError("TODO: implement delete logic")
+    use_case: FromDishka[DeleteUrlUseCase],
+):
+    """Remove a shortened URL."""
+    if await use_case.execute(
+        dto=DeleteUrlDTO(key=url_key),
+    ):
+        return {"deleted": True}
+
+    raise UrlNotFoundException(url_key=url_key)
