d66538557ed495b3a8aa2dec9fdaa2c6678e1ce7
mrKazzila <mrkazzila@gmail.com>
2026-01-26 22:02:53 +0300

    test: check

--- backend/src/test_check.py
+++ backend/src/test_check.py
@@ -5,6 +5,10 @@ from src.config.ioc.providers import PROVIDERS
 from src.presentation.grpc import GRPC_INTERCEPTORS, GRPC_SERVICES
 
 
+def new_function():
+    pass
+
+
 def main():
     get_server = server(
         is_reflection_enable=True,
@@ -12,6 +16,9 @@ def main():
         grpc_interceptors=GRPC_INTERCEPTORS,
         grpc_services=GRPC_SERVICES,
     )
+
+    r = new_function()
+    print(r)
     asyncio.run(get_server)
 
 
